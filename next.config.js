063867322e7903359d2f8872083d98af9de7a7a9
David L <dvdlq99@gmail.com>
2021-10-08 16:24:56 +0200

    changed backend url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    backendURL: "https://node-send-app.herokuapp.com/",
-    frontendURL: "https://gallant-blackwell-a34f30.netlify.app/"
+    backendURL: "https://node-send-app.herokuapp.com",
+    frontendURL: "https://gallant-blackwell-a34f30.netlify.app"
   }
 }
